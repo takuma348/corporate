f61f4a4e76323c93405dfdfb3cd7bfa4b2628b67
Takuma Ibe <go.ahead.mabuhay@gmail.com>
2020-03-07 12:30:35 +0900

    ファーストビュー差し替え

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-
-  //---------------------------------
+ //---------------------------------
   // Smooth Scrolling
   //---------------------------------
 
@@ -18,7 +17,9 @@ $(document).ready(function() {
 
     return false;
   });
+});
 
+$(document).ready(function() {
   //---------------------------------
   // Is-scrolled
   //---------------------------------
@@ -39,70 +40,76 @@ $(document).ready(function() {
 
 });
 
-//---------------------------------
-// AOS
-//---------------------------------
-AOS.init({
-  duration: 600,
-  easing: 'ease-out-quart',
-  once: true
+$(document).ready(function() {
+  //---------------------------------
+  // AOS
+  //---------------------------------
+  AOS.init({
+    duration: 600,
+    easing: 'ease-out-quart',
+    once: true
+  });
 });
 
+
+
+$(document).ready(function() {
 //---------------------------------
 // More btn
 //---------------------------------
 
-$(function () {
-  // 表示させる要素の総数をlengthメソッドで取得
-  var $numberListLen = $("#news-list .news-item").length;
-  // デフォルトの表示数
-  var defaultNum = 3;
-  // ボタンクリックで追加表示させる数
-  var addNum = 3;
-  // 現在の表示数
-  var currentNum = 0;
+  $(function () {
+    // 表示させる要素の総数をlengthメソッドで取得
+    var $numberListLen = $("#news-list .news-item").length;
+    // デフォルトの表示数
+    var defaultNum = 3;
+    // ボタンクリックで追加表示させる数
+    var addNum = 3;
+    // 現在の表示数
+    var currentNum = 0;
 
-  $("#news-list").each(function () {
-    // moreボタンを表示し、closeボタンを隠す
-    $(this).find("#more-btn").show();
-    // $(this).find("#close_btn").hide();
+    $("#news-list").each(function () {
+      // moreボタンを表示し、closeボタンを隠す
+      $(this).find("#more-btn").show();
+      // $(this).find("#close_btn").hide();
 
-    // defaultNumの数だけ要素を表示
-    // defaultNumよりインデックスが大きい要素は隠す
-    $(this).find(".news-item:not(:lt(" + defaultNum + "))").hide();
+      // defaultNumの数だけ要素を表示
+      // defaultNumよりインデックスが大きい要素は隠す
+      $(this).find(".news-item:not(:lt(" + defaultNum + "))").hide();
 
-    // 初期表示ではデフォルト値が現在の表示数となる
-    currentNum = defaultNum;
+      // 初期表示ではデフォルト値が現在の表示数となる
+      currentNum = defaultNum;
 
-    // moreボタンがクリックされた時の処理
-    $("#more-btn").click(function () {
-      // 現在の表示数に追加表示数を加えていく
-      currentNum += addNum;
-      
-      // 現在の表示数に追加表示数を加えた数の要素を表示する
-      $(this).parents().find(".news-item:lt(" + currentNum + ")").slideDown();
+      // moreボタンがクリックされた時の処理
+      $("#more-btn").click(function () {
+        // 現在の表示数に追加表示数を加えていく
+        currentNum += addNum;
 
-      // 表示数の総数よりcurrentNumが多い=全て表示された時の処理
-      if ($numberListLen <= currentNum) {
-        // 現在の表示数をデフォルト表示数へ戻す
-        currentNum = defaultNum;
-        // インデックス用の値をセット
-        indexNum = currentNum - 1;
+        // 現在の表示数に追加表示数を加えた数の要素を表示する
+        $(this).parents().find(".news-item:lt(" + currentNum + ")").slideDown();
 
-        // moreボタンを隠し、closeボタンを表示する
-        $("#more-btn").hide();
-        // $("#close_btn").show();
+        // 表示数の総数よりcurrentNumが多い=全て表示された時の処理
+        if ($numberListLen <= currentNum) {
+          // 現在の表示数をデフォルト表示数へ戻す
+          currentNum = defaultNum;
+          // インデックス用の値をセット
+          indexNum = currentNum - 1;
 
-        // closeボタンがクリックされた時の処理
-        // $("#close_btn").click(function () {
-        //   // デフォルト数以降=インデックスがindexNumより多い要素は非表示にする
-        //   $(this).parent().find(".news-item:gt(" + indexNum + ")").slideUp();
+          // moreボタンを隠し、closeボタンを表示する
+          $("#more-btn").hide();
+          // $("#close_btn").show();
 
-        //   // closeボタンを隠し、moreボタンを表示する
-        //   $(this).hide();
-        //   $("#more-btn").show();
-        // });
-      }
+          // closeボタンがクリックされた時の処理
+          // $("#close_btn").click(function () {
+          //   // デフォルト数以降=インデックスがindexNumより多い要素は非表示にする
+          //   $(this).parent().find(".news-item:gt(" + indexNum + ")").slideUp();
+
+          //   // closeボタンを隠し、moreボタンを表示する
+          //   $(this).hide();
+          //   $("#more-btn").show();
+          // });
+        }
+      });
     });
   });
 });
